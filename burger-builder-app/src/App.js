4b293ffc63f5daf2b542fd9ba1be024532a73e46
John <jkforsythe@hotmail.com>
2020-05-26 23:03:44 +0100

    created burger ingredient folders

--- burger-builder-app/src/App.js
+++ burger-builder-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>test</p>
-        </Layout> 
+          <BurgerBuilder/>
+      </Layout> 
     </div>
   );
 }
