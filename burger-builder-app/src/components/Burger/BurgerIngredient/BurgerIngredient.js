b73bb085e6ac2e2ce5674be2eeb29337053562ee
John <jkforsythe@hotmail.com>
2020-05-26 23:33:08 +0100

    cheese, bacon & salad ingredients added

--- burger-builder-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder-app/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -19,6 +19,16 @@ const burgerIngredient = (props) => {
         break;
       case ('meat'):
         ingredient = <div className={classes.Meat}></div>;
+        break;
+      case ('cheese'):
+        ingredient = <div className={classes.Cheese}></div>;
+        break;
+      case ('bacon'):
+        ingredient = <div className={classes.Bacon}></div>;
+        break;
+      case ('salad'):
+        ingredient = <div className={classes.Salad}></div>;
+        break;
     }
 };
 
