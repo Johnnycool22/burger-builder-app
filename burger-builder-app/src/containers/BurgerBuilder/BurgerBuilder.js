869e6e2143ffa781721fb923197af077f4fcfe0b
John <jkforsythe@hotmail.com>
2020-05-31 20:22:34 +0100

    purchase function added

--- burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 
 import Aux from '../../hoc/Auxillary';
 import Burger from '../../components/Burger/Burger';
@@ -23,7 +23,22 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
+    }
+
+    updatePurchaseState () {
+        const ingredients = {
+        ...this.state.ingredients
+        };
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+            this.setState({purchasable: sum > 0})
     }
 
     addIngredientHandler = (type) => {
@@ -70,6 +85,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice} />
           </Aux>
       );
