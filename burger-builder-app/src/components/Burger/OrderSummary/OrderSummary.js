a7b3d4aef3ffe3fe9293dcc679bc3a842def58e1
John <jkforsythe@hotmail.com>
2020-06-02 23:52:51 +0100

    total price and order buttons logic added

--- burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Aux from '../../../hoc/Auxillary';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
       const ingredientSummary = Object.keys(props.ingredients)
@@ -18,9 +19,10 @@ const orderSummary = (props) => {
               <ul>
                   {ingredientSummary}
               </ul>
-              <p>Continue to Checkout?</p>
-              <button>CANCEL</button>
-              <button>CONTINUE</button>
+                <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={props.purchasedContinued}>CONTINUE</Button>
           </Aux>
       )
 };
