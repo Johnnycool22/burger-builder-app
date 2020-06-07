fef76c5a49cade24888cb24349d33e50702ca927
John <jkforsythe@hotmail.com>
2020-06-07 21:05:49 +0100

    order summary improved, turned into class

--- burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxillary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-      const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log('[OrderSummary] DidUpdate');
+    }
+
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
       .map(igKey => {
       return (
             <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}: {props.ingredients[igKey]}</span>
+                <span style={{textTransform: 'capitalize'}}>{igKey}: {this.props.ingredients[igKey]}</span>
             </li> );
       });
 
-      return (
-          <Aux>
+        return (
+            <Aux>
               <h3>Your Order</h3>
               <p>A delicious burger with the following ingredients:</p>
               <ul>
                   {ingredientSummary}
               </ul>
-                <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
                 <p>Continue to Checkout?</p>
-                <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-                <Button btnType="Success" clicked={props.purchasedContinued}>CONTINUE</Button>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchasedContinued}>CONTINUE</Button>
           </Aux>
-      )
-};
+        );
+    }
+} 
 
-export default orderSummary;
+export default OrderSummary;
