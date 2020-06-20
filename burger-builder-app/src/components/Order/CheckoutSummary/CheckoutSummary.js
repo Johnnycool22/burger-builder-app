426504a841528a282221c11de057183f17690cd3
John <jkforsythe@hotmail.com>
2020-06-20 07:09:35 +0100

    checkout detail created

--- burger-builder-app/src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ burger-builder-app/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
+import classes from './CheckoutSummary.css';
 
 const checkoutSummary = (props) => {
     return (
-        <div className="">
+        <div className={classes.CheckoutSummary}>
             <hi>We hope it tastes well!</hi>
             <div style={{width: '300px', height: '300px', margin: 'auto'}}>
                 <Burger ingredients={props.ingredients}/>
