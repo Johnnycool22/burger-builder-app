54807d739e259678bd708865f70bd1c00288fc45
John <jkforsythe@hotmail.com>
2020-06-27 15:54:23 +0100

    order form dynamic element created

--- burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder-app/src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,57 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+                name: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Your Name'
+                    },
+                    value: ''
+                },
+                street: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Street'
+                    },
+                    value: ''
+                },
+                zipCode: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Post Code'
+                    },
+                    value: ''
+                },
+                country: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'text',
+                        placeholder: 'Country'
+                    },
+                    value: ''
+                },
+                email: {
+                    elementType: 'input',
+                    elementConfig: {
+                        type: 'email',
+                        placeholder: 'Your E-mail'
+                    },
+                    value: ''
+                },
+                deliveryMethod: {
+                    elementType: 'select',
+                    elementConfig: {
+                        options: [
+                            {value: 'fastest', displayValue: 'Fastest'},
+                            {value: 'cheapest', displayValue: 'Cheapest'},
+                        ]
+                    },
+                    value: ''
+                }
         },
         loading: false
     }
@@ -22,17 +68,7 @@ class ContactData extends Component {
         this.setState( { loading: true } );
          const order = {
              ingredients: this.props.ingredients,
-             price: this.props.price,
-             customer: {
-                 name: 'Mark todd',
-                 address: {
-                     street: 'Teststreet 1',
-                     zipCode: '41351',
-                     country: 'Ireland'
-                 },
-                 email: 'test@test.com'
-             },
-             deliveryMethod: 'fastest'
+             price: this.props.price            
          }
          axios.post('/orders.json', order)
              .then(response => {
