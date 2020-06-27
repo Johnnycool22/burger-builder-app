a89a3dcfffff02138bb2ccaacfeef2033d006a64
John <jkforsythe@hotmail.com>
2020-06-27 14:36:45 +0100

    input folder and js created

---
+++ burger-builder-app/src/components/UI/Input/Input.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const input = (props) => {
+  let inputElement = null;
+
+  switch (props.inputType) {
+      case ('input');
+        inputElement = <input />
+  }
+
+  return (
+      <div>
+        <label>{props.label}</label>
+      
+      </div>
+  );
+};
+
+export default input;
