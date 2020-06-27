6e7c0678bd5b9f44ceec6252408d58acc0c4ebd7
John <jkforsythe@hotmail.com>
2020-06-27 11:00:48 +0100

    get method for orders

--- burger-builder-app/src/containers/Orders/Orders.js
+++ burger-builder-app/src/containers/Orders/Orders.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
 
 class Orders extends Component {
+    componentDidMount() {
+        axios.get('/orders.json')
+    }
+
     render () {
         return (
             <div>
