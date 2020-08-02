ee2ea38efe85e33dde27f079217a0ea65ed2e44f
John <jkforsythe@hotmail.com>
2020-08-02 11:23:43 +0100

    Auth actions created

--- burger-builder-app/src/store/actions/actionTypes.js
+++ burger-builder-app/src/store/actions/actionTypes.js
@@ -1,3 +1,5 @@
+import { checkPropTypes } from "prop-types";
+
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
 export const SET_INGREDIENTS = 'SET_INGREDIENTS';
@@ -11,3 +13,7 @@ export const PURCHASE_INIT = 'PURCHASE_INIT';
 export const FETCH_ORDERS_START = 'FETCH_ORDERS_START';
 export const FETCH_ORDERS_SUCCESS = 'FETCH_ORDERS_SUCCESS';
 export const FETCH_ORDERS_FAIL = 'FETCH_ORDERS_FAIL';
+
+export const AUTH_START = 'AUTH_START';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = 'AUTH_FAIL';
