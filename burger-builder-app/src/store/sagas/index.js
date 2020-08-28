86d311a4e243e5043d1b6787618b0d2881662cd1
John <jkforsythe@hotmail.com>
2020-08-28 23:42:32 +0100

    1000 ms added to expiration time

--- burger-builder-app/src/store/sagas/index.js
+++ burger-builder-app/src/store/sagas/index.js
@@ -1,7 +1,7 @@
 import { takeEvery } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
-import { logoutSaga, checkAuthTimeoutSag, authUserSaga } from './auth';
+import { logoutSaga, checkAuthTimeoutSaga, authUserSaga } from './auth';
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
