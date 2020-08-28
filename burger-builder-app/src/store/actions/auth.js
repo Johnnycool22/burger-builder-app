cf1b166508d91ae8fda3589bb61a6c912ec49cdf
John <jkforsythe@hotmail.com>
2020-08-28 19:27:10 +0100

    actions logout succeed put into saga

--- burger-builder-app/src/store/actions/auth.js
+++ burger-builder-app/src/store/actions/auth.js
@@ -32,6 +32,12 @@ export const logout = () => {
     };
 };
 
+export const logoutSucceed = () => {
+        return {
+            type: actionTypes.AUTH_LOGOUT
+    };
+};
+
 export const checkAuthTimeout = (expirationTime) => {
     return dispatch => {
         setTimeout(() => {
